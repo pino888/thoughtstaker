6173661ae4b9a455ff2fb7035cccaea723336d4f
Piotr Daniel <p.daniel.london@gmail.com>
2020-12-13 18:14:26 +0000

    initital commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Thoughts Taker
+
+It's a type of a diary/notepad but with some differences to represent ones personal diary/golden thoughts. Although its primary function isn't for taking short time notes i.e shopping list, passwords etc it can be utilised for this too. The app was mainly designed for the writers, thinkers, dreamers and everyone who'd like to keep a journal.
+
+Each note receives a date stamp for the reference.
+
+The main difference is that once a note has been made, it cannot be deleted. However, should the note become irrelevant to tha author, it can be crossed out and likewise should it become of an importance again it can simply be put to its original state.
+
+Many writers get inspired at odd times and very often are struggling to find a scrap of paper to write their thoughts down and because these days almost everyone has their smartphone on them it's easy to make a note and not worry about losing it.
+
+The app is still in it's testing phase.
---
+++ main.js
@@ -0,0 +1,114 @@
+//localStorage.clear('logSave')
+//localStorage.clear('newestDiv')
+const input = document.querySelector('#input-field');
+const button = document.getElementById('save');
+let log = document.querySelector('.log');
+let currentDiv = document.getElementById("div0");
+const firstDateStamp = document.getElementById("date-stamp-0");
+let strike = document.querySelectorAll(".strike");
+const firstDate = new Date();
+firstDateStamp.innerHTML = firstDate;
+let numberOfEntries = 0;
+
+// For future development
+/*let savedData = {
+    content: log.innerHTML,
+    divNumber: numberOfEntries
+}*/
+
+let logSave = JSON.parse(localStorage.getItem("logSave"));
+let newestDiv = localStorage.getItem("newestDiv");
+
+if (logSave != null) {
+    log.innerHTML = logSave
+}
+
+if (newestDiv != null) {
+    numberOfEntries = newestDiv;
+    currentDiv = document.getElementById(`div${numberOfEntries}`);
+}
+
+addStrikeButton();
+
+function addElement () { 
+    const now = new Date();
+    const year = now.getFullYear();
+    const options = { month: 'long'};
+    const month = new Intl.DateTimeFormat('en-US', options).format(now);
+    const day = now.getDate();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    hour = addZero(hour);
+    minute = addZero(minute);
+    const dateStamp = day+' '+month+' '+year+' '+hour+':'+minute;
+    // create a new div element with datestamp and content
+    numberOfEntries++;
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("entry");
+    newDiv.id = `div${numberOfEntries}`;
+    const staticDiv = document.createElement("div");
+    staticDiv.classList.add("static");
+    const newSpan = document.createElement("span");
+    newSpan.id = `date-stamp-${numberOfEntries}`;
+    newSpan.innerHTML = dateStamp;
+    const newContent = document.createTextNode(input.value);
+    // create line through button
+    const newStrike = document.createElement("img");
+    newStrike.classList.add("strike");
+    newStrike.src = './resources/scribble.png';
+    
+    // add the text node, date stamp and button to the newly created div
+    newDiv.appendChild(staticDiv);
+    staticDiv.appendChild(newStrike);
+    staticDiv.appendChild(newSpan);
+    newDiv.appendChild(newContent);
+    input.value = '';
+    input.focus();
+  
+    // add the newly created element and its content with button into the DOM 
+    log.insertBefore(newDiv, currentDiv);
+    currentDiv = document.getElementById(newDiv.id);
+    addStrikeButton();
+
+    saveState();
+}
+
+function saveState() {
+    localStorage.setItem('logSave', JSON.stringify(log.innerHTML));
+    localStorage.setItem('newestDiv', numberOfEntries);
+}
+
+addZero = (time) => {
+    if(time<10) {
+        time = "0" + time;
+    }
+    return time;
+}
+
+button.onmouseup = addElement;
+
+returnKey = (event) => {
+    var x = event.which || event.keyCode;
+    if (x == 13) {
+        button.onmouseup();
+    }
+}
+
+function addStrikeButton() {
+    strike = document.querySelectorAll(".strike");
+    for (i = 0; i < strike.length; i++){
+        strike[i].onmouseup = strikeThrough;
+    }
+}
+
+function strikeThrough() {
+    if (this.parentElement.parentElement.dataset.state == 'true'){
+        this.parentElement.parentElement.style.textDecoration = 'none';
+        this.parentElement.parentElement.setAttribute("data-state", false);
+        saveState();
+    }else{
+        this.parentElement.parentElement.style.textDecoration = 'line-through';
+        this.parentElement.parentElement.setAttribute("data-state", true);
+        saveState();
+    }
+}
